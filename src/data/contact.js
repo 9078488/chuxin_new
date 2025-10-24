0b4cd6dd9412e716fd0217edcc3a222f5f73a2ac
Allan <gdkr100@163.com>
2025-10-24 11:17:57 +0800

    111

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,7 +2,7 @@ const contact = {
   company_name: '楚信环保科技（上海）有限公司',
   tel: '021-6485 2260',
   add: '上海市闵行区沪闵路443-445号9幢3楼301室（云境443）',
-  email: 'aales@chuxinsh.com',
+  email: 'sales@chuxinsh.com',
   website: 'https://www.chuxinsh.com/',
 }
 
