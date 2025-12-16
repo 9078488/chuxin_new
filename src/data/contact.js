c3ac2fb9825dbcad369acc632381586120901717
Allan <gdkr100@163.com>
2025-12-16 11:01:21 +0800

    change email

--- src/data/contact.js
+++ src/data/contact.js
@@ -2,7 +2,7 @@ const contact = {
   company_name: '楚信环保科技（上海）有限公司',
   tel: '021-3468 1851',
   add: '上海市闵行区沪闵路443-445号9幢3楼301室（云境443）',
-  email: 'bob.shi@chuxinsh.com',
+  email: ' bob.shi@chuxinsh.com',
   website: 'https://www.chuxinsh.com/',
 }
 
